032aa073d3b13f59d5a3a35603bfe4bdcbd0252c
Frank Calabrese <57340460+FrankCalabrese@users.noreply.github.com>
2021-10-22 09:05:27 -0500

    removed controller for reservations

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -48,6 +48,30 @@ const createUser = async({googleId, name}) =>{
     }
 }
 
+const createReservation = async({roomId, startDate, endDate}) =>{
+    try
+    {
+        await mongoose.connect(process.env.ATLAS_URI);
+
+        //const price = 
+        //const roomId = 
+        
+        const reservation = new Reservation({roomId, startDate, endDate /*, price*/});
+
+        //await addReservationReferenceToRoom(roomId, reservation._id);
+
+        await reservation.save();
+
+        mongoose.connection.close();
+        return {status: 201, message: `reservation successfully created!`};
+    }
+    catch(err)
+    {
+        mongoose.connection.close();
+        throw {status: 500, error: 'Could not create reservation'};
+    }
+}
+
 module.exports = 
 {
     getCustomerReservationsById,
